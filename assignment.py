177c53af054866cfaf8e414116b38b2d05acb30c
Chris Atkinson <46910324+chrisatk@users.noreply.github.com>
2023-11-03 08:53:56 -0400

    Update assignment.py
    Scaffolded steps

--- assignment.py
+++ assignment.py
@@ -1,1 +1,21 @@
-Add the price of the pizza along with the price of the topping. Then calculate the tax by multiplying the total number by 1.13
+#Add the price of the pizza along with the price of the topping. 
+
+# Collect the pizza size from the user
+pizza_size = input()
+
+# Collect the number of toppings from the user
+toppings_number = input()
+
+# Calculate the pizza cost based on the size that the user asked for
+pizza_cost = 
+
+# Calculate the toppings cost based on the number that the user asked for
+toppings_cost = 
+
+# Calculate the cost of the order before tax
+subtotal = 
+  
+#Then calculate the tax by multiplying the total number by 1.13
+
+
+# Summarize the order for the user and tell them how much they owe
