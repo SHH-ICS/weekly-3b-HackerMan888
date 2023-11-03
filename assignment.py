62f34aa6e49f623cd982d4620b0293a7ebafe229
HackerMan888 <144242698+HackerMan888@users.noreply.github.com>
2023-11-03 08:58:21 -0400

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,21 +1,24 @@
 #Add the price of the pizza along with the price of the topping. 
 
 # Collect the pizza size from the user
-pizza_size = input()
+pizza_size = input(Large)
 
 # Collect the number of toppings from the user
-toppings_number = input()
+toppings_number = input(Two)
 
 # Calculate the pizza cost based on the size that the user asked for
-pizza_cost = 
+pizza_cost = 6.00
 
 # Calculate the toppings cost based on the number that the user asked for
-toppings_cost = 
+toppings_cost = 1.75
 
 # Calculate the cost of the order before tax
-subtotal = 
+subtotal = 7.75
   
 #Then calculate the tax by multiplying the total number by 1.13
 
+1
 
 # Summarize the order for the user and tell them how much they owe
+
+8.75
